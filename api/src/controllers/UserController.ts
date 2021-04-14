e03e52aa533ef5e3b87a9fb8f9a0d74ae5520600
Azzedine <54241944+Arsero@users.noreply.github.com>
2021-04-14 22:40:34 +0200

    Create UserController.ts

---
+++ api/src/controllers/UserController.ts
@@ -0,0 +1,44 @@
+import User from './../entities/User';
+import express, { Request, Response, Router } from 'express';
+import HttpException from '../common/exceptions/HttpException';
+import UserService from '../services/UserService';
+import SendError from '../common/utils/SendError';
+
+export default class UserController {
+	public router: Router;
+	private userService: UserService;
+
+	constructor() {
+		this.userService = new UserService();
+		this.router = express.Router();
+		this.routes();
+	}
+
+	public login(req: Request, res: Response) {
+		try {
+			const [email, password] = [req.body.email, req.body.password];
+			this.userService.login(email, password);
+
+			res.status(200).send();
+		} catch (error) {
+			throw new HttpException(400, error.message);
+		}
+	}
+
+	public signup = async (req: Request, res: Response) => {
+		try {
+			const user = req.body as User;
+			console.log(this);
+			const newUser = await this.userService.create(user);
+
+			res.status(200).send(newUser);
+		} catch (error) {
+			SendError(error, res);
+		}
+	};
+
+	public routes() {
+		this.router.post('/login', this.login);
+		this.router.post('/signup', this.signup);
+	}
+}
