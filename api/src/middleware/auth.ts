959d79cb7fa6f0e819a3a6130da7f753d9e22528
Azzedine <54241944+Arsero@users.noreply.github.com>
2021-04-14 22:40:50 +0200

    Update auth.ts

--- api/src/middleware/auth.ts
+++ api/src/middleware/auth.ts
@@ -1,27 +1,31 @@
+import SendError from '../common/utils/SendError';
 import * as express from 'express';
 import jwt from 'jsonwebtoken';
+import HttpException from '../common/exceptions/HttpException';
 
 export const auth = (
 	req: express.Request,
 	res: express.Response,
 	next: express.NextFunction
 ) => {
-	const authHeader = req.headers.authorization;
+	try {
+		const authHeader = req.headers.authorization;
 
-	if (authHeader) {
-		const token = authHeader.split(' ')[1];
+		if (authHeader) {
+			const token = authHeader.split(' ')[1];
 
-		jwt.verify(token, process.env.SECRET_KEY, (err, username) => {
-			if (err) {
-				return res.sendStatus(403);
-			}
+			jwt.verify(token, process.env.SECRET_KEY, (err, username) => {
+				if (err) {
+					throw new HttpException(401, 'Not authorized');
+				}
 
-			req.username = username;
-			next();
-		});
-	} else {
-		res.status(401).send({
-			error: 'Not authorized',
-		});
+				req.username = username;
+				next();
+			});
+		} else {
+			throw new HttpException(400, 'Bad header');
+		}
+	} catch (error) {
+		SendError(error, res);
 	}
 };
