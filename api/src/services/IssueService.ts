0eb82071a219958489910d2ed72b9f625be48af4
Azzedine <54241944+Arsero@users.noreply.github.com>
2021-04-16 21:40:43 +0200

    Update IssueService.ts

--- api/src/services/IssueService.ts
+++ api/src/services/IssueService.ts
@@ -1,22 +1,72 @@
-import { getRepository } from 'typeorm';
-import { Issue } from '../entities/Issue';
+import { getRepository, Repository } from 'typeorm';
+import EntityException from '../common/exceptions/EntityException';
+import Issue from '../entities/Issue';
+import Project from '../entities/Project';
 
-export class IssueService {
-	private issueRepository = getRepository(Issue);
+export default class IssueService {
+	private issueRepository: Repository<Issue>;
+	private projectRepository: Repository<Project>;
 
-	async findByProject(projectId: string) {
-		return this.issueRepository.find({ project: { id: projectId } });
+	constructor() {
+		this.issueRepository = getRepository(Issue);
+		this.projectRepository = getRepository(Project);
 	}
 
-	async create(issue: Issue) {
-		return this.issueRepository.create(issue);
+	public async get(projectId: string, userId: number) {
+		if (!projectId || !userId) {
+			throw new EntityException('Missing parameter');
+		}
+
+		const project = await this.projectRepository.findOne(projectId, {
+			relations: ['owner', 'users', 'issues'],
+		});
+
+		if (!project) {
+			throw new EntityException('Project not found');
+		}
+
+		if (
+			project.owner.id != userId &&
+			!project.users.find((user) => user.id == userId)
+		) {
+			throw new EntityException('User not in this project');
+		}
+
+		return project.issues;
 	}
 
-	async update(issue: Issue) {
+	public async create(issue: Issue, projectId: string) {
+		if (!issue || !issue.title || !projectId) {
+			throw new EntityException('Missing parameter');
+		}
+
+		const project = await this.projectRepository.findOne({ id: projectId });
+		if (!project) {
+			throw new EntityException('Project not found');
+		}
+
+		issue.lastUpdate = new Date();
+		issue.project = project;
+
+		return this.issueRepository.save(issue);
+	}
+
+	public async update(id: number, issue: Issue, userId: number) {
+		const projectDb = await this.projectRepository.findOne(id, {
+			relations: ['owner', 'users'],
+		});
+
+		if (!projectDb || projectDb.owner.id != userId) {
+			throw new EntityException('Project does not exist');
+		}
+
+		project.id = id;
+		return this.projectRepository.update(id, project);
+		
 		return this.issueRepository.update({ id: issue.id }, issue);
 	}
 
-	async remove(issueId: number) {
+	public async remove(issueId: number) {
 		return this.issueRepository.delete(issueId);
 	}
 }
